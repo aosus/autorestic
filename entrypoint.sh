2d3dd4e014b019775870270c4cf822462975e1a9
FarisZR <35614734+fariszr@users.noreply.github.com>
2022-04-29 00:10:09 +0300

    Add docker support

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,14 @@ then
     echo "CRON_CONFIG_DIR is set, cron enabled"
     crond -f -L /var/log/cron.log
 fi
+
+if [ -n "$DOCKER_ENABLED" ]
+then
+    echo "DOCKER_ENABLED is set, Installing docker-cli"
+    apk add --no-cache docker-cli
+    echo "installing docker compose V2"
+    mkdir -p ~/.docker/cli-plugins/ 
+    wget https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -O ~/.docker/cli-plugins/docker-compose 
+    chmod +x ~/.docker/cli-plugins/docker-compose
+    docker compose version
+fi
