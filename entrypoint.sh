b08b47f075ccd88ad4fe9a0214ca0518f364658f
FarisZR <35614734+fariszr@users.noreply.github.com>
2022-06-04 15:02:56 +0300

    Check for Docker_ENABLED  on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-autorestic $AUTORESTIC_INITIAL_ARGS
-if [ -n "$CRON_CONFIG_DIR" ]
-then
-    echo "CRON_CONFIG_DIR is set, cron enabled"
-    crond -f -L /var/log/cron.log
-fi
 
 if [ -n "$DOCKER_ENABLED" ]
 then
@@ -15,4 +9,12 @@ then
     wget https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -O ~/.docker/cli-plugins/docker-compose 
     chmod +x ~/.docker/cli-plugins/docker-compose
     docker compose version
+fi
+
+autorestic $AUTORESTIC_INITIAL_ARGS
+
+if [ -n "$CRON_CONFIG_DIR" ]
+then
+    echo "CRON_CONFIG_DIR is set, cron enabled"
+    crond -f -L /var/log/cron.log
 fi
